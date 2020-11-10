b10189554d5d1d8791550480d03733200b71c049
Genki8723 <genking0403@yahoo.co.jp>
2020-11-10 21:44:25 +0900

    表示変更

--- main.js
+++ main.js
@@ -6,19 +6,43 @@
 
   const inputTask = document.getElementById('inputTask');
   const addButton = document.getElementById('addButton');
+  
   const todoList = document.getElementById('todo-list');
-  const statusradioBtn = document.getElementsByName('status');
+
+  const workingBtn = document.getElementById('workingBtn');
+  const doneBtn = document.getElementById('doneBtn');
+
+  const statusRadioBtn = document.getElementsByName('status');
+  
+
+
+  const todoChange = (tr, switchBtn) => {    
+          tr.classList.remove('hidden');
+          if(workingBtn.checked === true) {
+               tr.classList.remove('hidden');
+               if(switchBtn.textContent === '完了') {
+                    tr.classList.add('hidden');
+               }
+          }else if (doneBtn.checked === true) {
+               tr.classList.remove('hidden');
+               if(switchBtn.textContent === '作業中') {
+                    tr.classList.add('hidden');
+               }
+          }
+     }    
   //ラジオボタンの状態をセット
   const statusType = {
     ALL: 'allBtn',
     WORKING: 'workingBtn',
     DONE: 'doneBtn',
   };
+
   //削除を実行する関数
    const delList = index => {
       todos.splice(index, 1);
       showTodo();
-    }
+   }
+  
   //削除ボタンを作成する関数
   const createDelBtn = (index) => {
     const delButton = document.createElement('button');
@@ -44,6 +68,7 @@
       }
     })
 
+  
     const todo = {status: statusType.ALL};
 
     todo.value = todoTable;
@@ -61,6 +86,8 @@
     }
 
     todos.forEach((todo, index) => {
+      
+
       const tr = document.createElement('tr');
       const idNumber = document.createElement('td');
       const tdTable = document.createElement('td');
@@ -69,9 +96,19 @@
 
       idNumber.textContent = index + 1;
       tdTable.textContent = todo.value;
-      const switchButton = todo.state;
+      const switchBtn = todo.state; 
+
 
       const delButton = createDelBtn(index);
+      const disButton = document.getElementsByName('status');
+
+      for (let i = 0; i < disButton.length; i++) {
+        disButton[i].addEventListener('change', function () {
+          todoChange(tr, switchBtn);
+        });
+      }
+
+      todoChange(tr, switchBtn);
 
       del.appendChild(delButton);
       state.appendChild(todo.state);
